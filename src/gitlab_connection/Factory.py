dfbba083f92aae9936cb32ee8136ba18009e0b57
David Kuda <david.kuda@pm.me>
2021-11-10 21:39:48 +0100

    Create the class "ConnectionCreator"

---
+++ src/gitlab_connection/Factory.py
@@ -0,0 +1,14 @@
+from .GitLabConnection import GitLabConnection
+from .GitLabProjectVariables import GitLabProjectVariables
+
+
+class ConnectionCreator(GitLabConnection):
+    def __init__(self, gitlab_access_token, api_base_url) -> None:
+        super().__init__(gitlab_access_token, api_base_url)
+    
+    def create_project_variables_object(self, project_num: int):
+        return GitLabProjectVariables(
+            self.gitlab_access_token,
+            self.api_base_url,
+            project_num
+        )
