056cf58ddb80249634b4283c5c98e5806cd60f97
David Kuda <david.kuda@pm.me>
2021-11-10 22:38:39 +0100

    Truncate name of method

--- src/gitlab_connection/Factory.py
+++ src/gitlab_connection/Factory.py
@@ -6,7 +6,7 @@ class ConnectionCreator(GitLabConnection):
     def __init__(self, gitlab_access_token, api_base_url) -> None:
         super().__init__(gitlab_access_token, api_base_url)
     
-    def create_project_variables_object(self, project_num: int):
+    def create_project_vars_obj(self, project_num: int):
         return GitLabProjectVariables(
             self.gitlab_access_token,
             self.api_base_url,
