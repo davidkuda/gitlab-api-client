c62a0e1cf1d53bf598a24d783230e196717eab77
David Kuda <david.kuda@pm.me>
2021-11-18 15:38:48 +0100

    refactor: Use new classes in main()

--- src/set_project_variables.py
+++ src/set_project_variables.py
@@ -1,60 +1,47 @@
 # Author: David Kuda
 # Creation Date: 1. October 2021
-#
-# Description:
-#
-# Interacts with the GitLab api to create new variables
-# that can be used in gitlab ci.
 
 import os
 from pprint import pprint
 
-from GitLabConnection import GitLabConnection
-from utils import transform_vars_data_structure
+from gitlab_connection.Factory import ConnectionCreator
+from gitlab_connection.utils import transform_vars_data_structure
 
 
 def main():
 
+    connection_creator = connect_to_at()
+
+    # Define the project of which you want to copy the existing vars
+    src_repository = 841
+
+    src_repository_vars_obj = connection_creator.create_project_vars_obj(src_repository)
+    src_repository_vars = src_repository_vars_obj.get_project_vars()
+    
+    # Define the projects of which you want to set the variables
+    dest_repository = 2782
+    dest_repository = connection_creator.create_project_vars_obj(dest_repository)
+    dest_repository.copy_vars_from_other_project(src_repository_vars)
+
+def connect_to_at():
     # Make sure to have your personal GitLab Access Token as env var
     GITLAB_ACCESS_TOKEN = os.environ.get('GITLAB_PERSONAL_API_TOKEN')
-
-    # Create a GitLan Connection
-    gitlab_connection = GitLabConnection(
+    
+    # Create a GitLab Connection
+    connection_creator = ConnectionCreator(
         gitlab_access_token=GITLAB_ACCESS_TOKEN,
         api_base_url='https://gitlab.alexanderthamm.com/api/v4'
     )
-    
-    # Optionally: Search for the numbers of a project
-    # gitlab_connection.list_groups()
-    # gitlab_connection.list_projects_of_group(362)
-    # gitlab_connection.search_project('jupyter')
+    return connection_creator
 
-    # Define the project of which you want to copy the existing vars
-    jupyter_images = 578
 
-    jupyter_images_vars_object = gitlab_connection \
-        .create_project_variables_object(jupyter_images)
-    jupyter_images_vars = jupyter_images_vars_object.get_project_vars()
-    
-    vars = transform_vars_data_structure(jupyter_images_vars)
-    
-    # Define the projects of which you want to set the variables
-    metadata_extraction = 464 
-    mp4_from_bag = 466 
-    video_split_and_mask_creation = 473
-    
-    projects = [
-        metadata_extraction,
-        mp4_from_bag,
-        video_split_and_mask_creation    
-    ]    
-    
-    # Set the variables for each project
-    for project in projects:
-        project_variables = gitlab_connection \
-            .create_project_variables_object(project)
-        project_variables.set_project_variables(vars)
-    
+def search_project_num():
+    gitlab_connection = connect_to_at()
+    project_finder = gitlab_connection.create_project_finder()
+    project_finder.list_groups()
+    project_finder.list_projects_of_group(852)
+    project_finder.search_project('ecr-images')
+
 
 if __name__ == '__main__':
     main()
