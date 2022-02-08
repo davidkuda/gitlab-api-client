068e8f49cbeca4832b1277a8cf2cff0514157e7e
David Kuda <david.kuda@pm.me>
2022-02-08 18:31:57 +0100

    feat: Implement search for group id

--- src/gitlab_connection/project_finder.py
+++ src/gitlab_connection/project_finder.py
@@ -11,17 +11,6 @@ class ProjectFinder(GitLabConnection):
     def __init__(self, gitlab_access_token, api_base_url) -> None:
         super().__init__(gitlab_access_token, api_base_url)
 
-    def list_groups(self):
-            groups = self._get('/groups')
-            group_names_and_id = []
-            # pprint(groups[0].keys())
-            for group in groups:
-                group_names_and_id.append({
-                        "group_id": group.get('id'),
-                        "group_name": group.get('name')
-                    })
-            pprint(group_names_and_id)
-
     def list_projects_of_group(self, group_num):
         projects_of_group = self._get(f'/groups/{group_num}/projects?per_page=100')
         print(len(projects_of_group))
@@ -34,9 +23,21 @@ class ProjectFinder(GitLabConnection):
         pprint(project_names_and_ids)
 
     def search_group(self, name: str):
-        # Does not work, API has no search scope for group.
-        response = self._get(f'/search?scope=groups&search={name}')
-        pprint(response)
+        params = {
+            "per_page": 100,
+            "search": name,
+        }
+        groups = self._get('/groups', params)
+        group_names_and_id = []
+        pprint(groups[0].keys())
+        pprint(groups[3])
+        for group in groups:
+            group_names_and_id.append({
+                    "group_id": group.get('id'),
+                    "group_name": group.get('name'),
+                    "full_path": group.get('full_path'),
+                })
+        pprint(group_names_and_id)
 
     def search_project(self, name: str):
         response = self._get(f'/search?scope=projects&search={name}')
