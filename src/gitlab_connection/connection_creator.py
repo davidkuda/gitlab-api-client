6f0cf6abedf617b26f9cc133d99b83e9d5de65c6
David Kuda <david.kuda@pm.me>
2022-02-08 16:42:47 +0100

    Rename arg to "project_id"

--- src/gitlab_connection/connection_creator.py
+++ src/gitlab_connection/connection_creator.py
@@ -10,7 +10,7 @@ class ConnectionCreator(GitLabConnection):
     def __init__(self, gitlab_access_token, api_base_url) -> None:
         super().__init__(gitlab_access_token, api_base_url)
     
-    def create_project_connection(self, project_num: int):
+    def create_project_connection(self, project_id: int):
         return GitLabProject(
             self.gitlab_access_token,
             self.api_base_url,
